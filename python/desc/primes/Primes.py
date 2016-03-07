39fe54256138efadca9067b168851c91ea97bb2d
Jim Chiang <jchiang@slac.stanford.edu>
2016-03-06 20:16:45 -0800

    initial commit

---
+++ python/desc/primes/Primes.py
@@ -0,0 +1,15 @@
+"""
+Module to test for primes.
+"""
+
+
+def is_prime(number):
+    "Test if number is prime."
+
+    if number == 1:
+        return False
+
+    for factor in range(2, number):
+        if number % factor == 0:
+            return False
+    return True
---
+++ tests/test_Primes.py
@@ -0,0 +1,33 @@
+"""
+Unit tests for Primes package
+"""
+import unittest
+from desc.primes import is_prime
+
+class PrimesTestCase(unittest.TestCase):
+    """
+    TestCase class for is_prime function.
+    """
+    def test_one_is_not_prime(self):
+        "Test that one is not prime."
+        self.assertFalse(is_prime(1))
+
+    def test_two_is_prime(self):
+        "Test that two is prime."
+        self.assertTrue(is_prime(2))
+
+    def test_three_is_prime(self):
+        "Test that three is prime."
+        self.assertTrue(is_prime(3))
+
+    def test_four_is_not_prime(self):
+        "Test that four is not prime."
+        self.assertFalse(is_prime(4))
+
+    def test_float_raises_type_error(self):
+        "Test that a non-integer returns a TypeError."
+        self.assertRaises(TypeError, is_prime, 3.14159)
+
+
+if __name__ == '__main__':
+    unittest.main()
